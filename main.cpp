39cd0b315888f6a2e32511d617779969c087ff5a
theamazingmeep <michaelthesteuber@gmail.com>
2023-11-20 11:12:22 -0500

    Initial commit - added main.cpp and cmakelist.txt

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+
+/* TODO - Open the raw data and write it to a vector
+ * Parameters - fileIn
+ * Returns - 512 x 512 vector
+ */
+
+
+/* TODO - Compute distance from point A to point B
+ * Parameters - Height values vector, two sets of coordinates (x, y) provided by user
+ * Returns - Surface distance in meters
+ */
+
+
+/* TODO - Calculate difference in surface distance between pre and post eruption data
+ * Parameters - Two distance values
+ * Returns - Difference between the two values
+ */
+
+ int main() {
+    return 0;
+}
