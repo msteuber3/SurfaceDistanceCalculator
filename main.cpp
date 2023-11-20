4787f0598dbb1888c22ceec28d7fb5c2dad9ff87
theamazingmeep <michaelthesteuber@gmail.com>
2023-11-20 11:47:53 -0500

    Added writeFile() and a test print statement in main, as well as a gitignore with the clion project files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-#include <stdio.h>
+#include<fstream>
+using namespace std;
 
-/* TODO - Open the raw data and write it to a vector
- * Parameters - fileIn
- * Returns - 512 x 512 vector
- */
+
+vector<vector<int>> writeFile(const char* filePath){
+    fstream fileIn(filePath);
+    if(!filePath){
+        perror("Error: Could not open file");
+    }
+    int col = 512;
+    int row = 512; //Consider basing this on the size of the image files
+    vector<vector<int>> heightData(col, vector<int>(row));
+
+
+    vector<char> rawData(row * col);     // Writes the raw data from the .data file into a vector rawData
+    fileIn.read(rawData.data(), static_cast<long>(rawData.size()));
+
+    for(int i = 0; i < col; i++){           //Casts the data from rawData and writes it to 2D vector heightData
+        for(int j = 0; j < row; j++){
+            heightData[i][j] = static_cast<unsigned char>(rawData[i * col + j]);
+        }
+    }
+    return heightData;
+}
 
 
 /* TODO - Compute distance from point A to point B
@@ -19,5 +37,12 @@
  */
 
  int main() {
-    return 0;
+    vector<vector<int>> heightData = writeFile("/Users/michael/Documents/Programming/SurfaceDistanceCalculator/pre.data");
+    for(int i = 0; i < heightData.size(); i++){
+        for(int j = 0; j < heightData[0].size(); j++){
+            printf("%i",heightData[i][j]);
+            printf("%s", " ");
+        }
+        printf("%s","\n");
+    }
 }
