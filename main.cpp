12ae53e1d7e901ead0e2b8255d01f9bbb48a9b32
theamazingmeep <michaelthesteuber@gmail.com>
2023-11-20 17:25:20 -0500

    Added calculateSurfaceDistance; right now, it calculates the vector of pairs of coordinates between the two provided points. Also included a test in main.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 using namespace std;
 
-
+//TODO - Document this function properly
 vector<vector<int>> writeFile(const char* filePath){
     fstream fileIn(filePath);
     if(!filePath){
@@ -29,6 +29,47 @@ vector<vector<int>> writeFile(const char* filePath){
  * Parameters - Height values vector, two sets of coordinates (x, y) provided by user
  * Returns - Surface distance in meters
  */
+vector<pair<int, int>> calculateSurfaceDistance(vector<vector<int>> heightData, vector<int>coordOne, vector<int>coordTwo){
+    vector<pair<int, int>> heightsInPath;
+    int x = coordOne[0];
+    int y = coordOne[1];
+    int x2 = coordTwo[0];
+    int y2 = coordTwo[1];
+    float slope = static_cast<float>(y2 - y) / static_cast<float>(x2 - x);
+    if(slope < 0){
+        x = coordTwo[0];
+        y = coordTwo[1];
+        x2 = coordOne[0];
+        y2 = coordOne[1];
+        slope = static_cast<float>(y2 - y) / static_cast<float>(x2 - x);
+    }
+    int dx = x2 - x;                                         //Use Bresenham's line algorithm to find all the coordinates in between the two points
+    int dy = y2 - y;
+    int D = 2 * dy - dx;
+    if(slope < 1 && slope > -1){
+        while(x <= x2){
+            heightsInPath.insert(heightsInPath.end(), make_pair(x, y));
+            if(D > 0){
+                y++;
+                D = D - 2*dx;
+            }
+            D = D +2*dy;
+            x++;
+        }
+    }
+    else {
+        while (y <= y2) {
+            heightsInPath.insert(heightsInPath.end(), make_pair(x, y));
+            if (D > 0) {
+                x++;
+                D = D - 2 * dy;
+            }
+            D = D + 2 * dx;
+            y++;
+        }
+    }
+    return heightsInPath;
+}
 
 
 /* TODO - Calculate difference in surface distance between pre and post eruption data
@@ -38,11 +79,22 @@ vector<vector<int>> writeFile(const char* filePath){
 
  int main() {
     vector<vector<int>> heightData = writeFile("/Users/michael/Documents/Programming/SurfaceDistanceCalculator/pre.data");
-    for(int i = 0; i < heightData.size(); i++){
-        for(int j = 0; j < heightData[0].size(); j++){
-            printf("%i",heightData[i][j]);
-            printf("%s", " ");
+    vector<int> one {0, 1};
+    vector<int> two {6, 4};
+    vector<pair<int, int>> surfaceTest = calculateSurfaceDistance(heightData, one, two);
+
+        for (auto iter = surfaceTest.begin(); iter != surfaceTest.end(); iter++)
+        {
+            cout << iter->first << iter->second << endl;
         }
-        printf("%s","\n");
-    }
-}
+    cout << floor(3.00) << endl;
+    //    for(int i = 0; i < heightData.size(); i++){
+//        for(int j = 0; j < heightData[0].size(); j++){
+//            printf("%i",heightData[i][j]);
+//            printf("%s", " ");
+//        }
+//        printf("%s","\n");
+//    }
+    return 0;
+
+ }
